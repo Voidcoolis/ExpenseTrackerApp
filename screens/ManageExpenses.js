5bb5b4bfbc39b7d8c09b9315b0a48c256c02f79f
Bonjov Hima <bonjovhima@hotmail.de>
2025-05-25 22:17:32 +0200

    adding a delete button and styling it

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,22 +1,50 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { StyleSheet, View } from "react-native";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
 
-function ManageExpenses({route, navigation}) {
-//to ectract the expenseId from the route params
-const editedExpenseId = route.params?.expenseId; // optional chaining to avoid errors if expenseId is not provided
-const isEditing = !!editedExpenseId; // convert to boolean, true if editedExpenseId exists
+function ManageExpenses({ route, navigation }) {
+  //to ectract the expenseId from the route params
+  const editedExpenseId = route.params?.expenseId; // optional chaining to avoid errors if expenseId is not provided
+  const isEditing = !!editedExpenseId; // convert to boolean, true if editedExpenseId exists
 
-useLayoutEffect(() => {
-  navigation.setOptions({
-  title: isEditing ? "Edit Expense" : "Add Expense"
-});
-}, [navigation, isEditing]);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  function deleteExpenseHandler() {}
 
   return (
-    <Text>
-        Manage Expenses Screen
-    </Text>
+    <View style={styles.screenContainer}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPressBtn={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
   );
-}   
+}
 
 export default ManageExpenses;
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary50,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
